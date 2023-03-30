adb066e4189b7a970c79bd4f989edf18c1554c21
Tobias Waslowski <tobias.waslowski@thoughtworks.com>
2023-03-30 12:09:15 +0200

    refactor

--- src/app.py
+++ src/app.py
@@ -1,15 +1,13 @@
 import os
-from datetime import datetime, timezone
-from typing import Any
 
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, Application
 from telegram.ext import filters as Filters
-import logging
 import openai
+import logging
 
-from src.session import Session
+from src.session import get_user_session, Session, sessions
 
 # setup
 load_dotenv()
@@ -18,7 +16,6 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 # configure logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-sessions = []
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -26,36 +23,9 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-def _get_user_session(user_id: int) -> Session | None:
-    for session in sessions:
-        if session.user_id == user_id:
-            if session.expires_at > datetime.now(timezone.utc):
-                return session
-            # also manages session, should probably be in a separate function
-            else:
-                logging.info(f"Session for user {user_id} expired, removing it from the list")
-                sessions.remove(session)
-    return None
-
-
-def _create_new_session(update: Update) -> Session:
-    session = Session(user_id=update.effective_user.id,
-                      created_at=update.message.date,
-                      messages=[])
-    sessions.append(session)
-    return session
-
-
 async def prompt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user_id = update.effective_user.id
-    logging.info(f"Received message: {update.message.text} from user {user_id}")
-    session = _get_user_session(user_id)
-    if session:
-        logging.info(f"Found active session for user {user_id}")
-    else:
-        logging.info(f"Creating new session for user {user_id}")
-        session = _create_new_session(update)
-    session.messages.append({"role": "user", "content": update.message.text})
+    logging.info(f"Received message: {update.message.text} from user {update.effective_user.id}")
+    session = get_user_session(update)
 
     openai_response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
--- src/session.py
+++ src/session.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+import logging
+from telegram import Update
+
+sessions = []
 
 
 @dataclass
@@ -10,3 +14,26 @@ class Session:
 
     def __post_init__(self):
         self.expires_at = self.created_at + timedelta(minutes=30)
+
+
+def get_user_session(update: Update) -> Session:
+    user_id = update.effective_user.id
+    for session in sessions:
+        if session.user_id == user_id:
+            if session.expires_at > datetime.now(timezone.utc):
+                logging.info(f"Session for user {user_id} found")
+                return session
+            # also manages session, should probably be in a separate function
+            else:
+                logging.info(f"Session for user {user_id} expired, removing it from the list")
+                sessions.remove(session)
+    return _create_new_session(update)
+
+
+def _create_new_session(update: Update) -> Session:
+    session = Session(user_id=update.effective_user.id,
+                      created_at=update.message.date,
+                      messages=[])
+    sessions.append(session)
+    logging.info(f"Created new session for user {update.effective_user.id}")
+    return session

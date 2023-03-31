066b8d5340ede5668d348a643221669bf67f44ae
Tobias Waslowski <tobias.waslowski@thoughtworks.com>
2023-03-31 09:52:32 +0200

    refactor

--- src/handlers.py
+++ src/handlers.py
@@ -1,3 +1,5 @@
+import os
+
 from telegram import Update
 from telegram.ext import ContextTypes, CallbackContext
 from session import get_user_session
@@ -8,9 +10,18 @@ import logging
 
 async def handle_text_prompt(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     logging.info(f"Received message: {update.message.text} from user {update.effective_user.id}")
-    # send placeholder response
-    msg = await update.message.reply_text("Thinking...")
+    await handle_prompt(update)
 
+
+async def handle_voice_note(update: Update, context: CallbackContext) -> None:
+    logging.info(f"Received voice note from user {update.effective_user.id}")
+    transcript = await extract_text_from_audio(update, context)
+    update.message.text = transcript
+    await handle_prompt(update)
+
+
+async def handle_prompt(update: Update) -> None:
+    msg = await update.message.reply_text("Thinking...")
     session = get_user_session(update)
     session.messages.append({"role": "user", "content": update.message.text})
 
@@ -19,19 +30,24 @@ async def handle_text_prompt(update: Update, context: ContextTypes.DEFAULT_TYPE)
         messages=session.messages
     )
 
-    text = openai_response["choices"][0]["message"]["content"]
-    session.messages.append({"role": "assistant", "content": text})
-    logging.info(session)
-    await msg.edit_text(text=text)
+    response = openai_response["choices"][0]["message"]["content"]
+    session.messages.append({"role": "assistant", "content": response})
+    await msg.edit_text(text=response)
 
 
-async def handle_voice_note(update: Update, context: CallbackContext) -> None:
-    logging.info(f"Received voice note from user {update.effective_user.id}")
-    filename = f"tmp/{update.message.id}-voice_note"
+async def extract_text_from_audio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> str:
+    filename = f"./tmp/{update.message.id}-voice_note"
     # download audio and convert it to mp3
     audio_file = await context.bot.get_file(update.message.voice.file_id)
     await audio_file.download_to_drive(f"{filename}.ogg")
     AudioSegment.from_file(f"{filename}.ogg").export(f"{filename}.mp3")
-    # transcribe and return
-    transcript = openai.Audio.transcribe("whisper-1", open(f"{filename}.mp3", "rb"))
-    await update.message.reply_text(transcript["text"].encode().decode('utf-8'))
+    transcript = openai.Audio.transcribe("whisper-1", open(f"{filename}.mp3", "rb"))["text"].encode().decode('utf-8')
+    # cleanup
+    os.remove(f"{filename}.ogg")
+    os.remove(f"{filename}.mp3")
+    return transcript
+
+
+def is_prompt(transcript: str) -> object:
+    # contains deals with punctuation
+    return transcript.split(" ")[0].lower().__contains__("prompt")

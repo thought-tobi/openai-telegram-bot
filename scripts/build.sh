37e476c7196b9eedd01eb55490403f93e1be5dc7
Tobias Waslowski <tobias.waslowski@thoughtworks.com>
2023-04-05 18:19:02 +0200

    update scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-# create a virtual environment
-python3 -m venv venv
+# check if venv folder exists
+if [ ! -d "venv" ]; then
+    # create virtual environment
+    python3 -m venv venv
+fi
+# activate virtual environment
 source venv/bin/activate
 
 # install dependencies
@@ -10,6 +14,3 @@ pip install -r requirements.txt
 # create directory for writing images or audio files
 mkdir -p tmp
 chmod 755 tmp
-
-# pull mongodb docker image
-docker pull mongo:latest
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+# check for ffmpeg and ffprobe
+if ! [ -x "$(command -v ffmpeg)" ]; then
+    echo 'Error: ffmpeg is not installed.' >&2
+    exit 1
+fi
+
 # get current directory path
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PYTHONPATH=$PYTHONPATH:$ROOT_DIR
 
+# run mongodb docker image
+docker run -d -p 27017:27017 --name mongodb mongo:latest
+
 # activate virtual environment
 source venv/bin/activate
 python src/app.py &
-
-# run mongodb docker image
-docker run -d -p 27017:27017 --name mongodb mongo:latest

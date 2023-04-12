2b89e3a52f01c7ad042d6a2ea90b0db4d341833a
Tobias Waslowski <tobias.waslowski@thoughtworks.com>
2023-04-12 19:17:32 +0200

    improve error message

--- src/handlers/text_handlers.py
+++ src/handlers/text_handlers.py
@@ -24,7 +24,9 @@ async def handle_reply(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
 async def handle_error(update: object, context: CallbackContext) -> None:
     logging.error(f"Update {update} caused error {context.error}")
-    await update.message.reply_text("I'm very sorry, an error occured.")
+    await update.message.reply_text("I'm very sorry, an error occured. This bot is currently in development, so this"
+                                    "is bound to happen. Try resetting your session by typing /reset. If the problem"
+                                    "persists, contact @violin_tobi.")
     raise context.error
 
 

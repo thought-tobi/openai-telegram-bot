13170c61218af8f68df1ed1ef06f44be330879e6
Tobias Waslowski <tobias.waslowski@thoughtworks.com>
2023-06-09 15:39:28 +0200

    fix handler configuration

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ from telegram.ext import ApplicationBuilder, MessageHandler, Application, Comman
 from telegram.ext import filters as Filters
 
 from src.handlers.audio_handlers import handle_voice_note
-from src.handlers.command_handlers import handle_help, handle_reset
+from src.handlers.command_handlers import handle_start, handle_reset
 from src.handlers.error_handler import handle_error
 from src.handlers.text_handlers import handle_text_message
 
@@ -22,7 +22,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 def init_app() -> Application:
     app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("help", handle_help))
+    app.add_handler(CommandHandler("start", handle_start))
     app.add_handler(CommandHandler("reset", handle_reset))
     app.add_handler(MessageHandler(Filters.TEXT, handle_text_message))
     app.add_handler(MessageHandler(Filters.VOICE, handle_voice_note))
